01f591a28993253a796fdbf40a7c8ddb8f6a7113
Dallen Hoyal <dallen.hoyal@gmail.com>
2019-07-31 11:34:30 -0600

    testing 123

---
+++ build/precache-manifest.eec9f06f5cc78998fe9e68cc961fbcf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06428575a60b6bd16463722d0ba5fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293afbe07de3d18aaf78",
+    "url": "/static/css/main.30faea16.chunk.css"
+  },
+  {
+    "revision": "7dd75ccd837660a3659e",
+    "url": "/static/js/2.1cd53c45.chunk.js"
+  },
+  {
+    "revision": "293afbe07de3d18aaf78",
+    "url": "/static/js/main.f3dedaa1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5df597e2677c5ed7a2125b09a54e2dfa",
+    "url": "/static/media/Nodesto-Caps-Condensed-Bold.5df597e2.otf"
+  },
+  {
+    "revision": "e3f3c6957440986c5f03cf9bf57b27cf",
+    "url": "/static/media/Nodesto-Caps-Condensed.e3f3c695.otf"
+  }
+]);
--- server/equipment.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-    console.log('got to getUserEquip')
-    const dbInstance = req.app.get('db')
-    dbInstance.get_user_equip(req.session.passport.user.id).then((userEquipment) => {
-        res.status(200).send(userEquipment)
-    }).catch(() => res.status(500).send('didnt get userEquipment'))
-}
